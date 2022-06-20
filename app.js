c9ed4c842367857429bdd536679a445e13381703
DietJoy <96573392+DietJoy@users.noreply.github.com>
2022-06-20 15:48:58 +0200

    Etape 1 OK , à controller

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-    res.json({ message: 'Requête reçue Olé !' }); 
+const mongoose = require('mongoose');
+
+//Connection de l'Api à la base de donnée Mongoose:
+mongoose.connect('mongodb+srv://joy:joy180687@cluster0.v9ywjaz.mongodb.net/Piiquante?retryWrites=true&w=majority')
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+//Middleware qui intercepte toutes les requetes qui ont un contenu Json et le met à dipo dans req.body
+app.use(express.json());
+
+//*CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use((req, res, next) => {
+    res.json({ message: 'Requête reçue Olé !' });
+    next(); 
  });
 
 module.exports = app;
